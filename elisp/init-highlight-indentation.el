e30f03a2af890e54a494300c3c7e804c120ecfe6
liu yang <liuyangmy35@gmail.com>
2014-06-22 20:12:30 +0800

    use highlight-indentation-mode

--- elisp/init-highlight-indentation.el
+++ elisp/init-highlight-indentation.el
@@ -1,7 +1,7 @@
 (require-package 'highlight-indentation)
 
 (defun highlight-indentation-mode-hook ()
-  (highlight-indentation-current-column-mode)
+  (highlight-indentation-mode)
   (set-face-background 'highlight-indentation-face "#e3e3d3")
   (set-face-background 'highlight-indentation-current-column-face "#c3b3b3"))
 
