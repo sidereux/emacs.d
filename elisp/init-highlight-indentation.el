b6d2ae6da882e3c739ea542ec0940fd85b7cf20c
liu yang <liuyangmy35@gmail.com>
2014-06-24 17:26:28 +0800

    highlight indentation for python-mode

--- elisp/init-highlight-indentation.el
+++ elisp/init-highlight-indentation.el
@@ -6,5 +6,6 @@
   (set-face-background 'highlight-indentation-current-column-face "#c3b3b3"))
 
 (add-hook 'js2-mode-hook 'highlight-indentation-mode-hook)
+(add-hook 'python-mode-hook 'highlight-indentation-mode-hook)
 
 (provide 'init-highlight-indentation)
