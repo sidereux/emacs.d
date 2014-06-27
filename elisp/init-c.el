fbc93b6f3b04b9ea380a478ff2c703f29849875b
liu yang <liuyangmy35@gmail.com>
2014-06-27 15:09:18 +0800

    use elpy

--- elisp/init-c.el
+++ elisp/init-c.el
@@ -2,7 +2,7 @@
 (setq c-default-style "linux"
       c-basic-offset 4)
 
-;(setq-default c-basic-offset 4)
+
 
 
 (provide 'init-c)
--- elisp/init-python.el
+++ elisp/init-python.el
@@ -1,4 +1,9 @@
 ; set highlight indentation
 (add-hook 'python-mode-hook 'highlight-indentation-mode-hook)
 
+(require-package 'elpy)
+
+(elpy-enable)
+
+
 (provide 'init-python)
