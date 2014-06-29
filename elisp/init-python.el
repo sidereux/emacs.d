4bbc79c747b1b24406ebf6a89e70b12c5a3961ff
liu yang <liuyangmy35@gmail.com>
2014-06-29 15:00:59 +0800

    recover completion with RET

--- elisp/init-python.el
+++ elisp/init-python.el
@@ -5,5 +5,9 @@
 
 (elpy-enable)
 
+; elpy disabled completion with RET key
+; which is annoying
+(define-key ac-completing-map (kbd "RET") 'ac-complete)
+(define-key ac-completing-map (kbd "<return>") 'ac-complete)
 
 (provide 'init-python)
