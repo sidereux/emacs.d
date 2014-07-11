cc8be9a731b0a6fb86cccb2ab6ef064deb7856d2
liu yang <liuyangmy35@gmail.com>
2014-07-11 15:23:15 +0800

    use elpy, but disable flymake

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -6,6 +6,16 @@
 (add-hook 'python-mode-hook 'flycheck-mode)
 
 
+;install elpy
+;pip install elpy jedi
+(require-package 'elpy)
+
+(elpy-enable)
+
+; disable flymake
+(setq elpy-default-minor-modes (delete 'flymake-mode elpy-default-minor-modes))
+
+
 ; elpy disabled completion with RET key
 ; which is annoying
 (define-key ac-completing-map (kbd "RET") 'ac-complete)
