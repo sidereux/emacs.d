180a4599409cdfdde53b3e57afe27b026173731b
liu yang <liuyangmy35@gmail.com>
2014-07-21 08:25:44 +0800

    disable toolbar

--- lisp/init-common.el
+++ lisp/init-common.el
@@ -1,33 +1,36 @@
-; Save the cursor posision
+;; Save the cursor posision
 (require 'saveplace)
 (setq-default save-place t)
 
-; Scroll Smoothly
+;; Scroll Smoothly
 (setq redisplay-dont-pause t)
 (setq scroll-margin 5)
 (setq scroll-conservatively 1000)
-;(setq scroll-up-aggressively 0.01
-;      scroll-down-aggressively 0.01)
+;;(setq scroll-up-aggressively 0.01
+;;      scroll-down-aggressively 0.01)
+;
+;;(setq-default scroll-up-aggressively 0.01
+;;	      scroll-down-aggressively 0.01)
 
-;(setq-default scroll-up-aggressively 0.01
-;	      scroll-down-aggressively 0.01)
-
-; Change the size of a font for the current frame
+;; Change the size of a font for the current frame
 (set-face-attribute 'default (selected-frame) :height 150)
 
-; Line number
+;; Line number
 (global-linum-mode t)
 
-; Show Paren Mode
+;; Show Paren Mode
 (show-paren-mode t)
 
-; hilight current line
+;; hilight current line
 (global-hl-line-mode t)
 
-; Make the search case-sensitive
+;; Make the search case-sensitive
 (setq-default case-fold-search nil)
 
-; bind newline-and-indent to RET
+;; bind newline-and-indent to RET
 (define-key global-map (kbd "RET") 'newline-and-indent)
+
+;; disable toolbar
+(tool-bar-mode -1)
 
 (provide 'init-common)
