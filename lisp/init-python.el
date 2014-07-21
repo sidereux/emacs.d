0fcbcc194885cd7a47e43e506d3bfc736ec18a6c
liu yang <liuyangmy35@gmail.com>
2014-07-21 15:07:59 +0800

    delete elpy

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -6,19 +6,4 @@
 (add-hook 'python-mode-hook 'flycheck-mode)
 
 
-;install elpy
-;pip install elpy jedi
-(require-package 'elpy)
-
-(elpy-enable)
-
-; disable flymake
-(setq elpy-default-minor-modes (delete 'flymake-mode elpy-default-minor-modes))
-
-
-; elpy disabled completion with RET key
-; which is annoying
-(define-key ac-completing-map (kbd "RET") 'ac-complete)
-(define-key ac-completing-map (kbd "<return>") 'ac-complete)
-
 (provide 'init-python)
