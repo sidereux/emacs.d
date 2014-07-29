25568593a2d7fd1022f536735843b624fbf00f40
liu yang <liuyangmy35@gmail.com>
2014-07-29 11:37:26 +0800

    use jedi

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,9 +1,16 @@
-; set highlight indentation
+;; set highlight indentation
 (add-hook 'python-mode-hook 'highlight-indentation-mode)
 
-; enable flycheck for python-mode
-; pip install pylint
+;; enable flycheck for python-mode
+;; pip install pylint
 (add-hook 'python-mode-hook 'flycheck-mode)
 
+;; install jedi
+(require-package 'jedi)
+
+;; Install jedi server:
+;; M-x jedi:install-server
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
 
 (provide 'init-python)
