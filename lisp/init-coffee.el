c3316719db28d8268b99c937f5b2d062bc522791
liu yang <liuyangmy35@gmail.com>
2014-07-31 13:28:52 +0800

    change line indentation behaviour in coffee-mode

--- lisp/init-coffee.el
+++ lisp/init-coffee.el
@@ -7,4 +7,33 @@
 (add-hook 'coffee-mode-hook 'highlight-indentation-mode)
 (add-hook 'coffee-mode-hook 'flycheck-mode)
 
+;; Insert appropriate number of coffee-tab-with according to
+;; coffee-previous-indent and coffee-line-wants-indent.
+(defun my:coffee-indent-line ()
+  (interactive)
+
+  (back-to-indentation)
+  (let ((prev-indent (coffee-previous-indent)))
+
+    (coffee-insert-spaces prev-indent)
+    (if (coffee-line-wants-indent)
+        (progn
+          (coffee-insert-spaces coffee-tab-width)
+          (back-to-indentation)
+
+          ;; We're too far, remove all indentation.
+          (when (> (- (current-indentation) prev-indent) coffee-tab-width)
+            (backward-to-indentation 0)
+            (delete-region (point-at-bol) (point)))))))
+
+;; The original coffee-indent-line function could not work well with
+;; evil-mode.
+;;
+;; Use my:coffee-indent-line function instead of the original
+;; coffee-indent-line function.
+(defun my:coffee-mode-hook ()
+  (set (make-local-variable 'indent-line-function) 'my:coffee-indent-line))
+
+(add-hook 'coffee-mode-hook 'my:coffee-mode-hook)
+
 (provide 'init-coffee)
