4ce2e47cc23e362eff586ff7ac9fb0f003becba7
liu yang <liuyangmy35@gmail.com>
2014-08-07 18:35:13 +0800

    enable auto complete in markdown mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -6,4 +6,6 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+(add-hook 'markdown-mode-hook 'auto-complete-mode)
+
 (provide 'init-markdown)
