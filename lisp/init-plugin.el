2d2cc213883156693c6878ddb2e466c8582daa60
cobbled <liuyangmy35@gmail.com>
2016-04-17 12:01:42 +0800

    update shellcheck link

--- lisp/init-plugin.el
+++ lisp/init-plugin.el
@@ -72,8 +72,7 @@
 ;;; Flycheck - Modern on the fly syntax checking
 ;; Quick start : http://www.flycheck.org/manual/latest/Quickstart.html
 (require-package 'flycheck)
-;; Install ShellCheck https://github.com/koalaman/shellcheck/
-;; sudo apt-get install ShellCheck
+;; Install ShellCheck https://github.com/koalaman/shellcheck/releases
 (add-hook 'sh-mode-hook 'flycheck-mode)
 
 
