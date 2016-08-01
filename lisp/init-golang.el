5adde2fae5f410090a2f0b114939cb3edb099ef0
cobbled <liuyangmy35@gmail.com>
2016-08-01 19:29:33 +0800

    update init-golang.el comment

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -1,9 +1,10 @@
-;; go get -v -u github.com/golang/lint/golint
-;; go get -v -u github.com/kisielk/errcheck
+;; INSTALL go get -v -u github.com/golang/lint/golint
+;; INSTALL go get -v -u github.com/kisielk/errcheck
 
 ;; Set http_proxy and https_proxy environment variable when run 'go get'
 
-;; go get -u github.com/nsf/gocode
+;; INSTALL go get -u github.com/nsf/gocode
+;; INSTALL go get -u github.com/rogpeppe/godef
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 
 
