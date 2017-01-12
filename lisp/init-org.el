0f12e2cdfbcb86fe5de927757441e0b3c48fc9fb
liuyang <liuyangmy35@gmail.com>
2017-01-12 10:46:33 +0800

    set org-startup-truncated and org-startup-folded to nil delete unused package go-projectile

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,4 +1,7 @@
 ;; Install htmlize for syntax highlighting of org-mode export.
 (require-package 'htmlize)
 
+(setq-default org-startup-truncated nil)
+(setq-default org-startup-folded nil)
+
 (provide 'init-org)
