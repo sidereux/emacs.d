cc4bb62bef32fb6b0a6c6ace4cda1d9f0fb2dcae
cobbled <liuyangmy35@gmail.com>
2019-12-11 15:35:17 +0800

    add yapfify plugin

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -2,9 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
-(add-hook 'python-mode-hook #'lsp-deferred)
-(add-hook 'python-mode-hook
-          (lambda () (modify-syntax-entry ?_ "w" python-mode-syntax-table)))
+
+(use-package python
+  :config
+  (add-hook 'python-mode-hook #'lsp-deferred)
+  (add-hook 'python-mode-hook
+            (lambda () (modify-syntax-entry ?_ "w" python-mode-syntax-table)))
+  )
+
+(use-package yapfify
+  :ensure t
+  )
 
 (provide 'init-python)
 ;;; init-python.el ends here
