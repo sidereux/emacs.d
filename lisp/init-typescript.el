bdb8e29e7441080b7f0055bafbec193ce7614e8b
cobbled <liuyangmy35@gmail.com>
2020-10-28 19:29:54 +0800

    use typescript-mode and lsp

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -1,20 +1,12 @@
-;; echo 'registry=https://registry.npm.taobao.org' >> ~/.npmrc
-;; npm install -g typescript
-(require-package 'tide)
-
-(defun my:tide-setup()
-  (interactive)
-  (tide-setup)
-  (eldoc-mode +1))
-
-(setq typescript-indent-level 2)
-(setq tide-format-options '(:indentSize 2 :tabSize 2))
-
-;; make '_' a word character
-(add-hook 'typescript-mode-hook
-          (lambda () (modify-syntax-entry ?_ "w" typescript-mode-syntax-table)))
-
-(add-hook 'typescript-mode-hook '(lambda () (add-hook 'before-save-hook 'tide-format-before-save)))
-(add-hook 'typescript-mode-hook 'my:tide-setup)
+(use-package typescript-mode
+  :ensure t
+  :mode "\\.ts\\'"
+  :config
+  (add-hook 'typescript-mode-hook #'lsp-deferred)
+  (setq typescript-indent-level 2)
+  ;; make '_' a word character
+  (add-hook 'typescript-mode-hook '(lambda () (modify-syntax-entry ?_ "w" typescript-mode-syntax-table)))
+  (add-hook 'typescript-mode-hook '(lambda () (add-hook 'before-save-hook 'tide-format-before-save)))
+  )
 
 (provide 'init-typescript)
